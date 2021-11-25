59bd486ecb154eb9494b565b31e017116a18e69d
bang88 <sqibang@gmail.com>
2021-11-25 16:52:57 +0800

    chore: use global declaration

--- template/src/routes/Profile/index.tsx
+++ template/src/routes/Profile/index.tsx
@@ -7,13 +7,12 @@
 import { useNavigation } from '@react-navigation/core'
 import React from 'react'
 import { Button, Text, View } from 'react-native'
-import { AppScreenNavigationProp } from '../types'
 import styles from './profileStyle'
 
 export interface ProfileProps {}
 
 export const Profile: React.FunctionComponent<ProfileProps> = () => {
-  const { navigate } = useNavigation<AppScreenNavigationProp>()
+  const { navigate } = useNavigation()
   return (
     <View style={styles.profile} testID="profile_view">
       <Text testID="profile_text">This is Profile Component !</Text>
--- template/src/routes/types.ts
+++ template/src/routes/types.ts
@@ -16,3 +16,10 @@ export type AppScreenNavigationProp<
   BottomTabNavigationProp<RootStackParamList>,
   StackNavigationProp<RootStackParamList, RouteName>
 >
+
+// https://reactnavigation.org/docs/typescript/#specifying-default-types-for-usenavigation-link-ref-etc
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
